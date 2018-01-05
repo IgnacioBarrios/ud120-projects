423702ea2bde03e3f8a7879f856658117e143eeb
IgnacioBarrios <variosIBG@gmail.com>
2018-01-05 12:28:28 +0100

    Add Naive Bayes mini project

--- naive_bayes/nb_IBG.py
+++ naive_bayes/nb_IBG.py
@@ -29,17 +29,28 @@ from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 
 ### fit the classifier on the training features and labels
-#TODO
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
 ### use the trained classifier to predict labels for the test features
-clf.fit(features_train, labels_train)
+t0 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
 
 ### calculate and return the accuracy on the test data
 ### this is slightly different than the example, 
 ### where we just print the accuracy
 ### you might need to import an sklearn module
 accuracy = clf.score(features_test, labels_test)
-print accuracy
+print "accuracy 1: ",accuracy
+
+# Other way:
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+print "accuracy 2: ", accuracy
+
+
 
 #########################################################
 
---
+++ naive_bayes/studentCode.py
@@ -0,0 +1,19 @@
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+from classify import NBAccuracy
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab as pl
+
+
+def submitAccuracy():
+    accuracy = NBAccuracy(features_train, labels_train, features_test, labels_test)
+    
+    print(accuracy)
+    return accuracy
+    
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+submitAccuracy()
+
+
